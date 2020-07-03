396e4b50bb5b54121985c9a745f1cab4e297705c
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-03 17:05:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
